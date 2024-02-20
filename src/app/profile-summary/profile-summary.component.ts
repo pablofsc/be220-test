d842ee7ee5fb972ea80810649967b57ae11cd578
Pablo Felipe <pablofsc@outlook.com>
2024-02-20 19:37:31 -0300

    deploy to firebase

--- src/app/profile-summary/profile-summary.component.ts
+++ src/app/profile-summary/profile-summary.component.ts
@@ -9,6 +9,26 @@ export class ProfileSummaryComponent  implements OnInit {
 
   constructor() { }
 
-  ngOnInit() {}
+  ngOnInit() {
+    fetch('https://getmockuser-taktw2un5a-uc.a.run.app/')
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then(data => {
+        this.username = data.name;
+        this.level = data.level === 'purple' ? 'Nível Roxo' : undefined;
+      })
+      .catch(error => {
+        // Handle errors
+        console.error('There was a problem with the fetch operation:', error);
+      });
+
+  }
+
+  username: string | undefined;
+  level: string | undefined;
 
 }
